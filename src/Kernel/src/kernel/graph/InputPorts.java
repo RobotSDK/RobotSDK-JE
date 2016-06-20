500d0ffbdc36965226c1be8aa6e0e3647014c2b1
HMW-Alexander <alexanderhmw@gmail.com>
2016-06-20 13:09:27 +0800

    update kernel

--- src/Kernel/src/kernel/graph/InputPorts.java
+++ src/Kernel/src/kernel/graph/InputPorts.java
@@ -3,27 +3,52 @@ package kernel.graph;
 import kernel.proto.ProtoData;
 import kernel.proto.ProtoParam;
 
+import java.util.EventListener;
+import java.util.EventObject;
+import java.util.Vector;
 import java.util.concurrent.BlockingQueue;
 
-/**
- * Created by alexanderhmw on 6/18/16.
- */
-public class InputPorts extends Thread{
+class InputPort{
 
-    protected class InputPort{
-        public BlockingQueue<ProtoData> portDataBuffer;
-        public BlockingQueue<ProtoParam> portParamBuffer;
+    public int portId;
+    public BlockingQueue<ProtoData> portDataBuffer;
+    public BlockingQueue<ProtoParam> portParamBuffer;
+
+    public InputPort(int portId){
+        this.portId=portId;
     }
+}
+
+class InputPortEvent extends EventObject{
+
+    public InputPort inputPort;
+
+    public InputPortEvent(Object source, InputPort inputPort) {
+        super(source);
+        this.inputPort=inputPort;
+    }
+
+}
+
+interface InputPortListener extends EventListener{
+    public void inputPortEvent(InputPortEvent event);
+}
+
+public class InputPorts implements InputPortListener{
 
     protected int portNum;
-
-
-    @Override
-    public void run() {
-
-    }
+    protected Vector<InputPort> inputPorts=new Vector<>();
 
     public InputPorts(int portNum){
         this.portNum=portNum;
+        inputPorts.setSize(portNum);
+        for(int i=0;i<portNum;i++){
+            inputPorts.set(i,new InputPort(i));
+        }
+    }
+
+    @Override
+    public void inputPortEvent(InputPortEvent event) {
+
     }
 }
